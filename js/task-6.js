feef834665f2880a42435eee3e7cfbe954ad4ab2
Komrad71 <an.vasichkin@gmail>
2024-10-27 21:41:56 +0200

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -38,3 +38,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("#controls");
+const boxes = document.querySelector("#boxes");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+
+function createBoxes(amount) {
+  let size = 30;
+  const elements = [];
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    elements.push(div);
+    size += 10;
+  }
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+	document.querySelector("#boxes").innerHTML = "";
+}
+
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value); 
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("ERROR, number out of range!!! (1-100)");
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
